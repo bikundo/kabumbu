38a953bf3d4166512f7462fe4cced5d743d90f63
bikundo <binmonk@gmail.com>
2014-10-09 22:25:50 +0300

    Update database.php
    db changes

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'football_league',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'madchester',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
